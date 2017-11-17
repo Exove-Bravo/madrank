b74de2137b2d6a861586dfaaa7e2678cc74cb2ee
Kalle Kipinä <kalle.kipina@exove.fi>
2017-11-17 11:15:52 +0200

    Modify app.js to use websockets
    Use https://www.terlici.com/2015/11/26/realtime-node-expressjs-with-websockets.html as a base.

--- server/app.js
+++ server/app.js
@@ -1,29 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var http = require('http')
+  , express = require('express')
+  , app = express()
+  , server = http.createServer(app)
+  , io = require('socket.io')(server)
+  , count = 0
+  , logger = require('morgan');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,6 +25,19 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+io.on('connection', function (socket) {
+  count++;
+  io.emit('news', { msg: 'One more person is online', count: count });
+  socket.emit('private', { msg: 'Welcome you are the ' + count + ' person here' });
+  socket.on('private', function (data) {
+    console.log(data);
+  });
+  socket.on('disconnect', function() {
+    count--;
+    io.emit('news', { msg: 'Someone went home', count: count });
+  });
 });
 
 module.exports = app;
